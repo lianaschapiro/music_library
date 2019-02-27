7a9e015c0b0db4f07e4a2e03605a411a1d48a8fa
lianaschapiro <liana.schapiro@gmail.com>
2019-02-26 20:57:06 -0500

    begin methods

--- music.rb
+++ music.rb
@@ -1,1 +1,32 @@
+puts "Welcome to your music collection!"
+
+$albums = {}
+
+def add title, artist
+  album = "#{title} by #{artist}"
+  $albums[album] = "unplayed"
+  "added #{album}"
+end
+
+def play title
+
+end
+
+def show_all
+  $albums.each { | album, status | return "#{album} (#{status})" }
+end
+
+def show_unplayed
+
+end
+
+def show_unplayed_by artist
+end
+
+def quit
+
+end
+
+require 'irb'
+IRB.start
 
