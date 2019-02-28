c759b9a5d5a84f04b0e11bfab7192b197be97f79
lianaschapiro <liana.schapiro@gmail.com>
2019-02-28 16:24:42 -0500

    play function and checking for duplicates

--- music.rb
+++ music.rb
@@ -1,15 +1,27 @@
-puts "Welcome to your music collection!"
+"Welcome to your music collection!"
 
 $albums = {}
 
 def add title, artist
-  album = [title, artist]
-  $albums[album] = "unplayed"
-  "Added #{title} by #{artist}"
+  $albums.each do |album, status|
+    if album[0].downcase == title.downcase
+      return "You already have a song by that name."
+    end
+  end
+    album = [title, artist]
+    $albums[album] = "unplayed"
+    puts "Added \"#{title}\" by #{artist}"
 end
 
 def play title
-
+  $albums.each do |album, status|
+    if album[0].downcase == title.downcase
+      puts "You're listening to \"#{album[0]}\""
+      $albums[album] = "played"
+    else
+      "You don't have any songs by that name."
+    end
+  end
 end
 
 def show_all
@@ -24,7 +36,7 @@ def show_unplayed
   if $albums.value?("unplayed")
     $albums.each do |album, status|
       if status == "unplayed"
-        puts "#{album}"
+        puts "\"#{album[0]}\" by #{album[1]}"
       end
     end
   else
