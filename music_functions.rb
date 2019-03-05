d74760a3c017201fd7ab33d0e77185e1d8cd4644
lianaschapiro <liana.schapiro@gmail.com>
2019-03-04 22:29:13 -0500

    initial tests

--- music_functions.rb
+++ music_functions.rb
@@ -11,6 +11,7 @@ def add title, artist
     album = [title, artist]
     $albums[album] = "unplayed"
     puts "Added \"#{title}\" by #{artist}"
+    return album
 end
 
 # play songs
@@ -18,12 +19,12 @@ def play title
   if $albums.any?
     $albums.each do |album, status|
       if album[0].downcase == title.downcase
-        puts "You're listening to \"#{album[0]}\""
         $albums[album] = "played"
-      else
-        puts "You don't have any songs by that name."
+        puts "You're listening to \"#{album[0]}\""
+        return album => $albums[album]
       end
     end
+    puts "You don't have any songs by that name."
   else
     puts "You don't have any albums! You should add some."
   end
@@ -42,11 +43,14 @@ end
 def show_unplayed
   if $albums.any?
     if $albums.value?("unplayed")
+      unplayed = []
       $albums.each do |album, status|
         if status == "unplayed"
           puts "\"#{album[0]}\" by #{album[1]}"
+          unplayed.push(album)
         end
       end
+      return unplayed
     else
       puts "You have no unplayed albums"
     end
--- spec/music_functions_spec.rb
+++ spec/music_functions_spec.rb
@@ -8,7 +8,7 @@ describe "add" do
     end
 
      it "adds new album" do
-      expect(add("Ride the Lightning", "Metallica")).to eq nil
+      expect(add("Ride the Lightning", "Metallica")).to eq ["Ride the Lightning", "Metallica"]
     end
   end
 
@@ -33,7 +33,7 @@ describe "show all" do
     end
 
     it "shows no albums" do
-      expect(show_all()).to eq nil
+      expect(show_all).to eq nil
     end
   end
 
@@ -43,13 +43,14 @@ describe "show all" do
     end
 
     it "shows all albums" do
-      expect(show_all()).to eq ({["Ride the Lightning", "Metallica"]=>"unplayed", ["Pauls Boutique", "Beastie Boys"]=>"unplayed"})
+      expect(show_all).to eq ({["Ride the Lightning", "Metallica"]=>"unplayed", ["Pauls Boutique", "Beastie Boys"]=>"unplayed"})
     end
   end
 
 end
 
 describe "play" do
+
   context "when library is empty" do
     before do
       $albums = {}
@@ -66,7 +67,7 @@ describe "play" do
     end
 
     it "does not play song" do
-      expect(play("Ride the Lightning")).to eq ({["Pauls Boutique", "Beastie Boys"]=>"unplayed"})
+      expect(play("Ride the Lightning")).to eq nil
     end
   end
 
@@ -79,12 +80,67 @@ describe "play" do
       expect(play("Ride the Lightning")).to eq ({["Ride the Lightning", "Metallica"]=>"played"})
     end
   end
+
 end
 
-# describe "show unplayed" do
-#   it "shows unplayed albums" do
-#     expect(show_unplayed()).to eq "You have no unplayed albums"
-#   end
-# end
+describe "show unplayed" do
 
+  context "when library is empty" do
+    before do
+      $albums = {}
+    end
 
+    it "shows no albums" do
+      expect(show_unplayed).to eq nil
+    end
+  end
+
+  context "when library doesn't contain unplayed songs" do
+    before do
+      $albums = {["Ride the Lightning", "Metallica"]=>"played"}
+    end
+
+    it "shows no albums" do
+      expect(show_unplayed).to eq nil
+    end
+  end
+
+  context "when library contains unplayed songs" do
+    before do
+      $albums = {["Ride the Lightning", "Metallica"]=>"played", ["Pauls Boutique", "Beastie Boys"]=>"unplayed"}
+    end
+
+    it "shows unplayed albums" do
+      expect(show_unplayed).to eq [["Pauls Boutique", "Beastie Boys"]]
+    end
+  end
+
+end
+
+describe "show all by" do
+
+  context "when library is empty" do
+    before do
+      $albums = {}
+    end
+
+    it "shows no albums" do
+      expect(show_all_by("Beastie Boys")).to eq nil
+    end
+  end
+
+end
+
+describe "show unplayed by" do
+
+  context "when library is empty" do
+    before do
+      $albums = {}
+    end
+
+    it "shows no albums" do
+      expect(show_unplayed_by("Beastie Boys")).to eq nil
+    end
+  end
+
+end
